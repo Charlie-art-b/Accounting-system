6ba31a762f7dabfc28503213b72c28cda9ce861b
Kevin <kevinlaravalverde339@gmail.com>
2026-01-26 13:39:17 -0600

    Avance con el CRUD de Inventarios

---
+++ sistema-contable/app/Filament/Resources/InventoryProducts/Schemas/InventoryProductInfolist.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\InventoryProducts\Schemas;
+
+use Filament\Schemas\Schema;
+
+class InventoryProductInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
