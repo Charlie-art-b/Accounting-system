03a78c39da79f4466970dd2e2c715c3abb15a78b
Kevin <kevinlaravalverde339@gmail.com>
2026-02-26 16:52:21 -0600

    Pruebas de QA y Ajuste en inicio del proyecto y ajuste al iniciar proyecto

--- sistema-contable/app/Filament/Resources/Users/UserResource.php
+++ sistema-contable/app/Filament/Resources/Users/UserResource.php
@@ -19,23 +19,20 @@ use Filament\Tables\Table;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-     protected static ?string $navigationLabel = 'Usuarios';
-     protected static ?string $modelLabel = 'Usuario';
-    protected static ?string $pluralModelLabel = 'Usuarios';
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static ?string $recordTitleAttribute = 'Users';
-
-    protected static string|\UnitEnum|null $navigationGroup = 'CONFIGURACIÓN';
-
-    protected static ?int $navigationSort = 1;
-
-    protected static ?string $navigationLabel = 'Administración de Usuarios';
+    protected static ?string $navigationLabel = 'Administracion de Usuarios';
 
     protected static ?string $modelLabel = 'Usuario';
 
     protected static ?string $pluralModelLabel = 'Usuarios';
 
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
+
+    protected static ?string $recordTitleAttribute = 'Users';
+
+    protected static string|\UnitEnum|null $navigationGroup = 'CONFIGURACION';
+
+    protected static ?int $navigationSort = 1;
 
     public static function form(Schema $schema): Schema
     {
--- sistema-contable/routes/web.php
+++ sistema-contable/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PDFTestController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin/login');
 });
 
 Route::get('/test/estado-resultados/{cliente}', function (\App\Models\Customer $cliente) {
